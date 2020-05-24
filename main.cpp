54dd4494d74473ac543ccd512b7750c9d73a25d1
MakarovKO <kirill@example.com>
2020-05-24 15:40:01 +0300

    Добавление promt

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ vector<double>
 input_numbers (istream& in, size_t count)
 {
     vector <double> result(count);
-    for (int i=0; i<count; i++)
+    for (size_t i=0; i<count; i++)
     {
         in>>result[i];
     }
@@ -36,21 +36,30 @@ make_histogram(struct Input data)
     return bins;
 }
 Input
-read_input(istream& in) {
-    Input data;
-
-    cerr << "Enter number count: ";
+read_input(istream& in , bool promt)
+{
     size_t number_count;
-    in >> number_count;
-    cerr << "Enter numbers: ";
-    data.numbers = input_numbers(in, number_count);
-    cerr <<" Enter bin_count: ";
-    in >>data.bin_count;
+    Input data;
+    if(promt)
+    {
+        cerr << "Enter number count: ";
+        in >> number_count;
+        cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
+        cerr <<" Enter bin_count: ";
+        in >>data.bin_count;
+    }
+    else
+    {
+        in >> number_count;
+        data.numbers = input_numbers(in, number_count);
+        in >> data.bin_count;
+    }
     return data;
 }
 int main()
 {
-    const auto input = read_input(cin);
+    const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
     return 0;
