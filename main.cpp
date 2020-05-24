df942a0e40a5853c08944e609f5eecc006fae1d3
MakarovKO <kirill@example.com>
2020-05-24 16:09:18 +0300

    Загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -63,10 +63,13 @@ int main(int argc, char* argv[])
     curl_global_init(CURL_GLOBAL_ALL);
     if(argc>1)
     {
-        cout<<argc;
-        for (int i=0; i<argc;i++)
+        CURL *curl = curl_easy_init();
+        if(curl)
         {
-            cout<<argv[i];
+           CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL,argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
         }
         return 0;
     }
