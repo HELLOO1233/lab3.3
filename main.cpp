ba6c1e2ab999fb9ee377036ffd278bfd90c0a83a
MakarovKO <kirill@example.com>
2020-05-24 16:21:04 +0300

    сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <conio.h>
 #include "histogram.h"
 #include "svg.h"
+#include <string>
+#include <sstream>
 using namespace std;
 struct Input {
     vector<double> numbers;
@@ -58,29 +60,48 @@ read_input(istream& in , bool promt)
     }
     return data;
 }
-int main(int argc, char* argv[])
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 {
+    const size_t data_size = item_size * item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return 0;
+}
+Input
+download(const string& address) {
+    stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
-    if(argc>1)
-    {
-        CURL *curl = curl_easy_init();
+    CURL *curl = curl_easy_init();
         if(curl)
         {
-           CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL,argv[1]);
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL,address);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
             res = curl_easy_perform(curl);
             if (res != CURLE_OK)
             {
                 cout << curl_easy_strerror(res) << endl;
                 exit(1);
             }
-
             curl_easy_cleanup(curl);
         }
-        return 0;
+    return read_input(buffer, false);
+}
+int main(int argc, char* argv[]) {
+    Input input;
+    if (argc > 1)
+    {
+        input = download(argv[1]);
     }
-    const auto input = read_input(cin, true);
-    const auto bins = make_histogram(input);
+    else
+    {
+        input = read_input(cin, true);
+    }
+    const auto bins=make_histogram(input);
     show_histogram_svg(bins);
     return 0;
 }
+
